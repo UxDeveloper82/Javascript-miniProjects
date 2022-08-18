0e150540321cefa108ce34f22e20042443a94b58
Martin Fernandez <Electronicsmf@aol.com>
2022-08-17 21:13:52 -0400

    Dom-manipulation

---
+++ 13-Dom-manipulation/app.js
@@ -0,0 +1,27 @@
+function textChange() {
+    var inElement = document.getElementById("textIn");
+    var outElements = document.getElementsByTagName("p");
+    var headingElements = document.getElementsByClassName("heading");
+
+    for(var i =0; i < outElements.length; i++) {
+        var outItem = outElements[i];
+        headingElements[i].innerHTML = "Updating " + (i + 1) + 
+                                       " to " + inElement.value;
+        outItem.innerHTML = inElement.value;                               
+    }
+}
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991)
+
+const calcAge2 = function(birthYear) {
+   return 2037 - birthYear
+}
+ 
+const age2 = calcAge2(1991)
+
+
+console.log(age1, age2);
