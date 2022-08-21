7038d351860c7945ba2e1c0a0a6380587a393b30
Martin Fernandez <Electronicsmf@aol.com>
2022-08-21 14:44:52 -0400

    adding more widget

---
+++ 57-todoApp/script.js
@@ -0,0 +1,84 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
+
+function addTodo(e) {
+  e.preventDefault();
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  //Check Mark Button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fa fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  //Check Trash Button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fa fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  //Append To List
+  todoList.appendChild(todoDiv);
+
+  //Clear todo input value
+  todoInput.value = "";
+}
+function deleteCheck(e) {
+  const item = e.target;
+
+  //Delete TODO
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    //Animation
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+
+  //Chech Mark
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+ todos.forEach((todo) => {
+   const area = e.target.value;
+    switch (area) {
+        case "all": todo.style.display="flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+        default:
+    console.log('Sorry, we are out of ' + expr + '.');
+       }
+   });
+}
