7f145770fa7c12a3fb590559142dad29226adfe8
Martin Fernandez <Electronicsmf@aol.com>
2023-02-20 21:29:58 -0500

    adding icon for budgetapp

--- 38-Budgetjs/app.js
+++ 38-Budgetjs/app.js
@@ -123,11 +123,11 @@ var budgetController = (function() {
              // create html string with placeholder text
              if(type === 'inc'){
                  element = DOMStrings.incomeContainer;
-                 html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                 html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-circle-outline"></i></button></div></div></div>';
              
              }else if (type === 'exp'){
                  element = DOMStrings.expensesContainer;
-                 html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                 html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-circle-outline"></i></button></div></div></div>';
              }
              //replace the placeholder text with some actual data
               newHtml = html.replace('%id%', obj.id);
